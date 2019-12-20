358ae19410b272021f6afa30ba9c987e45aec425
yesefujiang <he123789456@gmail.com>
2019-12-20 16:22:31 +0800

    database

--- app.py
+++ app.py
@@ -20,26 +20,56 @@ class User(db.Model):  # 表名将会是user（自动生成，小写处理）
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))  # 电影标题
-    yaer = db.Column(db.String(4))  # 电影年份
+    year = db.Column(db.String(4))  # 电影年份
+
+@app.cli.command()
+@click.option('--drop',is_flag=True,help="Create after drop")
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
 
 
 # flask 默认启用多线程
 @app.route('/')
 def index():
+
     # return '<h1>欢迎来到我的watchlist！</h1> <img src="http://helloflask.com/totoro.gif" >'
-    return render_template("index.html", name=name, movies=movie)
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
 
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
 
-# 准备一些数据
-name = "Grey Li"
-movie = [
-    {"title": "My world", "year": 2019},
-    {"title": "My world", "year": 2019},
-    {"title": "My world", "year": 2019},
-    {"title": "My world", "year": 2019},
-    {"title": "My world", "year": 2019},
-    {"title": "My world", "year": 2019},
-]
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
 
 if __name__ == '__main__':
     app.run()
